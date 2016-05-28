780dbb3a055d9821d71d2a80e0948237d67f5a72
cgewecke <christophergewecke@gmail.com>
2016-05-28 14:45:49 -0700

    Bluetooth Tests TDD update

--- tests/bluetooth.spec.js
+++ tests/bluetooth.spec.js
@@ -2,76 +2,138 @@ describe('Bluetooth Service', function(){
 
    it('should define some constants for the events it publishes', function(){
       expect('test written').toBe(true);
-      // UNFUNDED
-      // PENDING
-      // POSTED
-      // POSTFAILURE
-
+   
    });
 
-   describe('findNode(uuid, major, minor)', function(){
+   // endpoints: returns a list of valid enpoint uuids
+   describe('endpoints', function(){
 
-      it('should return the means to connect to specified node from a server', function(){
+      it('should return a list of valid animist endpoint ids from (?)', function(){
          expect('test written').toBe(true);
       });
 
-      it('should handle server connection failure', function(){
-         expect('test written').toBe(true);
-      });
-
-      it('should handle lookup failure', function(){
-         expect('test written').toBe(true);
-      })
-   });
-
-   describe('connect(node)', function(){
-
-      it('should open a serialized bluetooth connection to the "node"', function(){
-         expect('test written').toBe(true);
-      });
-
-      it('should set a public flag indicating its connected', function(){
-         expect('test written').toBe(true);
-      });
-
-      it('it should handle connection failure', function(){
+      it('should verify that these values are correctly formatted', function(){
          expect('test written').toBe(true);
       });
 
    });
 
-   describe('disconnect()', function(){
+   // Listen (iOS): Initializes app to hear animist endpoint beacon signals & connect via BLE Returns promise
+   describe('listen([uuid, . . . (optional)])', function(){
 
-      it('should close the bluetooth connection to the current node', function(){
+      it('should validate passed argument as an array of correctly formatted uuids', function(){
+         expect('test written').toBe(true);
+      });
+      
+      it('should reject w/ error message if argument is invalid', function(){
          expect('test written').toBe(true);
       });
 
-      it('should set a public flag indicating its disconnected', function(){
+      it('should ask CB to listen for a discrete set of uuids if an array is passed to it', function(){
          expect('test written').toBe(true);
       });
 
-      it('it should handle disconnection errors', function(){
+      it('should listen for the full set of default endpoints if called without argument', function(){
+         expect('test written').toBe(true);
+      });
+
+      it('should check to see if BLE is enabled and initialize if necessary', function(){
+         expect('test written').toBe(true);
+      });
+
+      it('should reject on BLE initialization failure w/ message', function(){
+         expect('test written').toBe(true);
+      });
+
+      it('should reject on iBeacon requestAuthorization failure w/ message', function(){
          expect('test written').toBe(true);
       });
 
    });
 
-   describe('transmit( packet )', function(){
+   // authenticate()   (For iOS) Client passes signed beacon signal to endpoint and gets conf.
+   // message if address parses correctly.
+   describe('authenticate(node)', function(){
 
-      it('should verify that device is connected to a node', function(){
+      it('should open a BLE connection to the node at service "beaconUuid + 1', function(){
          expect('test written').toBe(true);
       });
 
-      it('should handle non-connection error state', function(){
+      it('should reject on BLE connection attempt failure w/ message', function(){
          expect('test written').toBe(true);
       });
 
-      it('should pass the packet to the node', function(){
+      it('should sign & write the beacon uuid to the peripheral auth characteristic', function(){
+         expect('test written').toBe(true);
+      });
+
+      it('should write its proximity to the proximity characteristic', function(){
+         expect('test written').toBe(true);
+      });
+
+      it('should reject on write characteristic failure', function(){
          expect('test written').toBe(true);
       });
 
    });
 
+   // hasTx() Queries current endpoint to see if there are tx's about this event and
+   // returns them to client. Tx's can require additional confirmations.
+   describe('hasTx()', function(){
+
+      it('should subscribe to the txVerify characteristic', function(){
+         expect('test written').toBe(true);
+      });
+
+      it('should reject if subscription fails or device disconnects w/message', function(){
+         expect('test written').toBe(true);
+      });
+
+      it('should resolve w/ array of txs if endpoint finds txs in log data', function(){
+         expect('test written').toBe(true);
+      });
+
+      it('should resolve false if no tx data was found', function(){
+         expect('test written').toBe(true);
+      });
+
+   });
+
+   // A second layer of authentication in which an app with an api key enumerated in the
+   // contract requests confirmation of location by passing Google GeoLocation data to 
+   // the endpoint. Endpoint needs to pull the contract to get the public api key. 
+   describe('authGeo(tx)', function(){
+
+      it('should write {tx, lat, lng} geoData to rawGeo characteristic', function(){
+         expect('test written').toBe(true);
+      });
+
+      it('should *then* write signed geoData to signedGeo characteristic', function(){
+         expect('test written').toBe(true);
+      });
+
+      it('should resolve if the endpoint accepts the data', function(){
+         expect('test written').toBe(true);
+      });
+
+      it('should reject on raw/signed Geo writes technical failure w/ message', function(){
+         expect('test written').toBe(true);
+      });
+
+      it('should reject on signed Geo write substantive failure w/ message', function(){
+         expect('test written').toBe(true);
+      });
+
+   });
+
+   describe('close()', function(){
+
+      it('should close the connection to the endpoint', function(){
+         expect('test written').toBe(true);
+      });
+
+   });
+      
    // Events
    describe('AnimistBluetooth:connect (Event)', function(){
 
@@ -87,7 +149,8 @@ describe('Bluetooth Service', function(){
       });
    });
 
-   describe('AnimistBluetooth:txSuccess (Event)', function(){
+   // Remote server confirmations/failures . . . . 
+   describe('AnimistCentral:txSuccess (Event)', function(){
 
       it('should fire on successful blockchain writes from the node', function(){
          expect('test written').toBe(true);
@@ -99,7 +162,7 @@ describe('Bluetooth Service', function(){
 
    });
 
-   describe('AnimistBluetooth:txStatus (Event)', function(){
+   describe('AnimistCentral:txStatus (Event)', function(){
 
       it('should fire on transaction status update from the node', function(){
          expect('test written').toBe(true);
@@ -110,7 +173,7 @@ describe('Bluetooth Service', function(){
       });
    });
 
-   describe('AnimistBluetooth:txFailure (Event)', function(){
+   describe('AnimistCentral:txFailure (Event)', function(){
 
       it('should fire on transaction failure msg from the node', function(){
          expect('test written').toBe(true);
